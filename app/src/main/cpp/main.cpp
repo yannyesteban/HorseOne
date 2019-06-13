3aa21b14a21c18bdde11cde1d1a9a98162117e60
Yanny Esteban <yannyesteban@hotmail.com>
2019-06-13 15:56:57 -0400

    por fin!!! opengl

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -4,12 +4,16 @@
 
 #include <EGL/egl.h>
 #include <GLES/gl.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 
 #include <android/sensor.h>
 #include <android/log.h>
 //#include "C:\sdk\ndk-bundle\sources\android\native_app_glue\android_native_app_glue.h"
 #include <android_native_app_glue.h>
 #include <cstring>
+#include <malloc.h>
+
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
@@ -25,7 +29,144 @@ struct engine {
 
     int32_t touchX;
     int32_t touchY;
+    GLuint programObject;
 };
+
+typedef struct
+{
+// Handle to a program object
+    GLuint programObject;
+} UserData;
+
+GLuint LoadShader( GLenum type, const char *shaderSrc)
+{
+    GLuint shader;
+    GLint compiled;
+// Create the shader object
+    shader = glCreateShader(type);
+    if(shader == 0)
+        return 0;
+// Load the shader source
+    glShaderSource(shader, 1, &shaderSrc, NULL);
+// Compile the shader
+    glCompileShader(shader);
+// Check the compile status
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+
+    if(!compiled)
+    {
+        GLint infoLen = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+        if(infoLen > 1)
+        {
+            char* infoLog = (char*) malloc(sizeof(char) * infoLen);
+            glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+            //esLogMessage("Error compiling shader:\n%s\n", infoLog);
+            free(infoLog);
+        }
+        glDeleteShader(shader);
+        return 0;
+    }
+    return shader;
+}
+
+int Init(struct engine* engine)
+{
+    //UserData *userData = esContext->userData;
+    const char* vShaderStr =
+            "attribute vec4 vPosition; \n"
+            "void main() \n"
+            "{ \n"
+            " gl_Position = vPosition; \n"
+            "} \n";
+    const char* fShaderStr =
+            "precision mediump float; \n"
+            "void main() \n"
+            "{ \n"
+            " gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \n"
+            "} \n";
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    GLuint programObject;
+    GLint linked;
+// Load the vertex/fragment shaders
+    vertexShader = LoadShader(GL_VERTEX_SHADER, vShaderStr);
+    fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fShaderStr);
+// Create the program object
+    programObject = glCreateProgram();
+    if(programObject == 0)
+        return 0;
+    glAttachShader(programObject, vertexShader);
+    glAttachShader(programObject, fragmentShader);
+// Bind vPosition to attribute 0
+    glBindAttribLocation(programObject, 0, "vPosition");
+// Link the program
+    glLinkProgram(programObject);
+// Check the link status
+    glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
+    if(!linked)
+    {
+        GLint infoLen = 0;
+        glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
+        if(infoLen > 1)
+        {
+            char* infoLog = (char*)malloc(sizeof(char) * infoLen);
+            glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
+            //esLogMessage("Error linking program:\n%s\n", infoLog);
+            free(infoLog);
+        }
+        glDeleteProgram(programObject);
+        return false;
+    }
+// Store the program object
+    engine->programObject = programObject;
+    glClearColor(0.0f, 0.0f, 0.28f, 1.0f);
+    return true;
+}
+
+void Draw(struct engine* engine)
+{
+    //UserData *userData = esContext->userData;
+    GLfloat vVertices1[] = {0.0f, 0.5f, 0.0f,
+                           -0.5f, -0.5f, 0.0f,
+                           0.5f, -0.5f, 0.0f};
+
+    GLfloat vVertices2[] = {1.0f, 1.0f, 0.0f,
+                           -1.0f, -1.0f, 0.0f,
+                           1.0f, -1.0f, 0.0f};
+
+    GLfloat vVertices[] = {-0.5f, 0.5f, 0.0f,
+                           -0.5f, -0.5f, 0.0f,
+
+                           0.0f, -0.5f, 0.0f,
+
+                           0.0f, -0.5f,0.0f,
+                           0.5f, -0.5f, 0.0f,
+                           0.5f,0.5f, 0.0f
+
+
+
+
+    };
+
+
+// Set the viewport
+    glViewport(0, 0, engine->width, engine->height);
+    //glViewport(400,400,400,400);
+// Clear the color buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+// Use the program object
+    glUseProgram(engine->programObject);
+// Load the vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
+    glEnableVertexAttribArray(0);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    eglSwapBuffers(engine->display, engine->surface);
+}
+
+
+
+
 
 /**
  * Initialize an EGL context for the current display.
@@ -96,7 +237,7 @@ int init_display(struct engine* engine) {
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
     glEnable(GL_CULL_FACE);
     glDisable(GL_DEPTH_TEST);
-    glViewport(0, 0, w, h);
+    //glViewport(0, 0, w, h);
 
     return 0;
 }
@@ -112,7 +253,9 @@ void draw_frame(struct engine* engine) {
 
     glClearColor(100,0,0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
-    eglSwapBuffers(engine->display, engine->surface);
+Init(engine);
+    Draw(engine);
+    //eglSwapBuffers(engine->display, engine->surface);
 }
 
 /**
